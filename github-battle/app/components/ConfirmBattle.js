5ed44fce15e2dabc9050c0ef6e6d2493dc3a2ef4
gionaufal <g10v45+github@gmail.com>
2016-08-12 09:02:03 -0300

    update github-battle until video 10. fully functional

--- github-battle/app/components/ConfirmBattle.js
+++ github-battle/app/components/ConfirmBattle.js
@@ -6,14 +6,10 @@ var Link = ReactRouter.Link;
 var UserDetails = require('./UserDetails');
 var UserDetailsWrapper = require('./UserDetailsWrapper');
 
-function puke(obj) {
-  return <pre>{JSON.stringify(obj, null, ' ')}</pre>
-};
-
 
 function ConfirmBattle (props) {
   return props.isLoading === true
-  ? <p>LOADING!</p>
+  ? <h1 className='text-center'>Loading...</h1>
   : <div className='jumbotron col-sm-12 text-center' style={styles.transparentBg}>
       <h1>Confirm Players</h1>
       <div className='col-sm-8 col-sm-offset-2'>
